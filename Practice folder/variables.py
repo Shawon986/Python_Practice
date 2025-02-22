8355f05828fa1301006d4eaa49edbd7d31f0757a
Shawon <shawonh986@gmail.com>
2025-02-22 15:56:09 +0800

    variables printing

---
+++ Practice folder/variables.py
@@ -0,0 +1,7 @@
+name = "Shawon"
+age = 24
+result = 5.00
+city = "Natore"
+print("This is "+ name +", I am ", age ,"years old, "+"At the age of ", age ,"I started learning python, "+"I got ", result ,"in my academic exam ." + " I live in "+city+ ".")
+
+# have to use "," when printing integer/float type variables
