a61d634fa391d6a806debd37048f42c52da99869
Shawon <shawonh986@gmail.com>
2025-02-22 17:13:25 +0800

    type casting

---
+++ Practice folder/typeCasting.py
@@ -0,0 +1,23 @@
+number01 = int(input("Type first number :"))
+number02 = int(input("Type second number :"))
+
+result = number01 + number02
+print("The result is :" ,result)
+
+result = number01 - number02
+print("The result is :" ,result)
+
+result = number01 * number02
+print("The result is :" ,result)
+
+result = number01 / number02
+print("The result is :" ,result)
+
+result = number01 % number02
+print("The result is :" ,result)
+
+result = number01 // number02
+print("The result is :" ,result)
+
+result = number01 ** number02
+print("The result is :" ,result)
