c04bbc30ea798c54e97af8722aa7f3e69c774a57
Shawon <shawonh986@gmail.com>
2025-02-22 17:30:36 +0800

    math related library function

---
+++ Practice folder/mathLibraryFunction.py
@@ -0,0 +1,11 @@
+# to use all math related library function, NEED to import all from math
+from math import*
+
+print(max(43,23))
+print(min(23,54))
+print(abs(-8))
+print(pow(4,8))
+print(sqrt(64))
+print(floor(43.567))
+print(ceil(43.567))
+
