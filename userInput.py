8abdc15dd3bd60fbd4cb2f9e800cde5541fea3c7
Shawon <shawonh986@gmail.com>
2025-02-22 17:01:57 +0800

    getting user input

---
+++ userInput.py
@@ -0,0 +1,9 @@
+name = input("Type your name :")
+age = input("Type your age :")
+gpa = input("Type your gpa :")
+
+print("Student information")
+print("------------------")
+print("Name : "+ name)
+print("Age : "+ age)
+print("GPA : "+ gpa)
